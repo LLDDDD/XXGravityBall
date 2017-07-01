1957776c9ac06a4fe232c1b40a56405963aa959e
xxg <xxg90s@qq.com>
2017-07-02 00:22:26 +0800

    实现动画；添加对应手势；添加调试对应参数

---
+++ GravityBall/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GravityBall
+//
+//  Created by xxg90s on 2017/7/1.
+//  Copyright © 2017年 xxg90s Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
